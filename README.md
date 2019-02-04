e4d9fa6145ab7e9b036e8f6726bbf490d1d7a80c
Hui lala <ctolentino@myhta.org>
2019-02-04 10:57:45 -1000

    problems added

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # LoopsBasics
 Practicing Loops &amp; Arrays after learning loops for the first time
+
+1. Complete all the problems #1-10 to get full credit in the app.js file
---
+++ app.js
@@ -0,0 +1,35 @@
+/* Done in wednesday class:
+1. What is a loop?
+
+2. What is the syntax of a loop?
+for(i = 0; i < 5; i++){
+
+}
+initializing
+terminating
+incrementing
+
+3. Give a few examples.
+*/
+
+//4. Create a loop that prints numbers from 0-10.
+
+//5. Create a loop that prints odd numbers from 1-9.
+
+//6. Create a loop that prints multiples of 3 from 3-15.
+
+//7. Create an array that lists 3 things you'd like to accomplish one day.
+
+//8. Create a loop that prints out each of the things you'd like to accomplish listed in your #7 array.  The loop should print out three
+//separate sentences, like this:
+//One day, I'd like to _element 1_.
+//One day, I'd like to _element 2_.
+//One day, I'd like to _element 3_.
+
+//9. Create an array that lists 3 places you'd like to visit one day.
+
+//10. Create a function (with a loop) that prints out each of the places you'd like to visit listed in your #9 array.  The function
+//should print out three separate sentences, like this:
+//One day, I'd like to go to _element 1_.
+//One day, I'd like to go to _element 2_.
+//One day, I'd like to go to _element 3_.
